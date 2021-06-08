f89417afc47541c619fb4339cd3f1e1c40e642b9
王涛 <wangtao338@jd.com>
2021-06-09 01:08:06 +0800

    feat: 管理资源 60%

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './icon.png'
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
 
   // lodash 在当前 script 中使用 import 引入
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', '你好', '天使'], ' ');
+  element.classList.add('hello')
+
+  const myIcon = new Image();
+  myIcon.src = Icon
+  document.body.appendChild(myIcon)
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
