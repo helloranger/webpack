83c63e0b3e4bec55de788e71aadf6e2842e06cc3
helloranger <rangwerzero@gmail.com>
2021-06-13 20:40:07 +0800

    feat: 环境变量

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
-  devtool: 'inline-source-map',
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
+module.exports = (env) => {
+  console.log('环境变量 goal:', env.goal)
+  console.log('环境变量 production:', env.production)
+
+  return {
+    entry: {
+      index: './src/index.js',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
+    },
+  }
+  
 };
